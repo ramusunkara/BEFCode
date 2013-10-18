e1bd9b780689b6c779c17e679250f9dd3ad0a155
Mark McEahern <mark@mceahern.com>
2013-10-18 13:20:23 -0500

    Whitespace

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -4,6 +4,7 @@ describe WelcomeController do
   describe 'GET index' do
     it 'renders the index template' do
       get :index
+
       expect(response).to render_template('index')
     end
   end
