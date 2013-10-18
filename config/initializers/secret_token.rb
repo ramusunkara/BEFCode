010cfa5918b6b00f97c9c2fbb72191430b520dfd
Mark McEahern <mark@mceahern.com>
2013-10-18 10:16:50 -0500

    rails new BEF -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BEF::Application.config.secret_key_base = 'c98ce2fb53e83cc600aeda8272a0c4b8b65c9c5e1668f2ce1cdd70fcceff300fd0d66ecfcee2fc05d651573f1b50a8e99aebfcdfd811659e7d7fd8c538364ef6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BEF::Application.config.session_store :cookie_store, key: '_BEF_session'
