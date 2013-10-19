78f92d5addafbc9cc74ec97d569a686927f3315c
Mark McEahern <mark@mceahern.com>
2013-10-19 16:02:55 -0500

    Add default scope to Events for sorting by start_at desc

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ActiveRecord::Base
   validates :starts_at, timeliness: true
   validates :title, presence: true
 
+  default_scope { order('events.starts_at desc') }
+
   def self.published
     where('publish_at is not null and publish_at <= ?', Time.now)
   end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -7,6 +7,13 @@ describe Event do
   it { should have_valid(:title).when 'x' }
   it { should_not have_valid(:title).when '', nil }
 
+  describe '#default_scope' do
+    it 'orders by starts_at desc' do
+      sql = Event.all.to_sql
+      expect(sql).to include %q(ORDER BY events.starts_at desc)
+    end
+  end
+
   describe '.published' do
     before { Timecop.freeze(Time.utc(2013, 10, 19, 13, 21, 33)) }
 
