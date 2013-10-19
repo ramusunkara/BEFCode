28ee9d6622446430fa863f1cbf3ea5700e91d789
Mark McEahern <mark@mceahern.com>
2013-10-19 15:28:26 -0500

    Add :with_tags trait to Person factory

--- spec/factories/person_factory.rb
+++ spec/factories/person_factory.rb
@@ -1,6 +1,21 @@
 FactoryGirl.define do
   factory :person do
+    ignore do
+      tags nil
+    end
+
     first_name 'Bob'
     last_name 'Jones'
+
+    trait :with_tags do
+      after(:create) do |instance, evaluator|
+        tags = evaluator.tags.map do |tag|
+          create :tag, title: tag
+        end
+
+        instance.tags << tags
+        instance.save!
+      end
+    end
   end
 end
