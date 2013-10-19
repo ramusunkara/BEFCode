aeb6abda520159117e35eb913c3eccb0209b7cc0
Mark McEahern <mark@mceahern.com>
2013-10-18 22:37:08 -0500

    Ignore admin from SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start 'rails' do
+  add_filter 'app/admin'
+end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
