c02f900d1ea9a09f5fd924c73042c74460838210
Mark McEahern <mark@mceahern.com>
2013-10-19 08:49:47 -0500

    Dynamically render titles

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,4 +1,7 @@
 class PeopleController < ApplicationController
+  expose(:raw_selected_tag) { Tag.where(title: tag_title).first }
+  expose(:selected_tag) { raw_selected_tag.decorate if raw_selected_tag }
+  expose(:tags) { Tag.all.decorate }
   expose(:tag_title) { params[:tag_title] }
   expose(:people) { Person.for_tag_title(tag_title).decorate }
 
