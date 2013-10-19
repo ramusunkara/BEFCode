ea047e144d073c29afa6b0c47c67e0ca69735695
Mark McEahern <mark@mceahern.com>
2013-10-19 08:49:11 -0500

    Seed Tag data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+titles = %q(
+  Entrepreneurs
+  Executives
+  Investors
+  Lawyers
+  Investment Bankers
+  Consultants
+  Faculty
+  Philanthropists
+)
+
+titles.split("\n").each do |title|
+  title = title.strip
+  next unless title.present?
+  Tag.find_or_create_by(title: title)
+end
