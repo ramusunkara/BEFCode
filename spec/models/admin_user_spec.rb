ebda5b3fcff5e181423c8fb90de7690cce7681ba
Mark McEahern <mark@mceahern.com>
2013-10-20 11:46:15 -0500

    Add test for AdminUser

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe AdminUser do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'includes the expected Devise modules' do
+    expected = [
+      Devise::Models::DatabaseAuthenticatable,
+      Devise::Models::Recoverable,
+      Devise::Models::Rememberable,
+      Devise::Models::Trackable,
+      Devise::Models::Validatable
+    ]
+
+    expected.each do |expected_klass|
+      match = described_class.ancestors.find { |klass| klass == expected_klass }
+      expect(match).not_to be_nil
+    end
+  end
 end
