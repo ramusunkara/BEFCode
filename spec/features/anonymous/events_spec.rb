e9d22dcde1ff38c283b7aa7a3156a76cbb36d66c
Philip Crawford <philipharry@gmail.com>
2013-10-21 11:28:21 -0500

    Update published test based on change from date to boolean

--- spec/features/anonymous/events_spec.rb
+++ spec/features/anonymous/events_spec.rb
@@ -16,18 +16,14 @@ feature 'Anonymous visits' do
 
   context 'when they visit the Events page' do
     before do
-      Timecop.freeze(Time.utc(2013, 10, 19))
-
-      create :event, title: 'Published a day ago', publish_at: 1.day.ago
-      create :event, title: 'Publish a day from now', publish_at: 1.day.from_now
+      create :event, title: 'Published', published: true
       create :event, title: 'Not published'
     end
 
-    scenario 'they only see events published before now' do
+    scenario 'they only see events published' do
       visit events_path
 
-      expect(page).to have_content 'Published a day ago'
-      expect(page).not_to have_content 'Published a day from now'
+      expect(page).to have_content 'Published'
       expect(page).not_to have_content 'Not published'
     end
   end
