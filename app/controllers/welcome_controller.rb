c3f6d05334a1db77317036bb2bcfa8b01e31a5f1
Philip Crawford <philipharry@gmail.com>
2013-10-21 20:39:02 -0500

    Use partials for displaying past and upcoming events on home and events pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   expose(:board_members) { Person.mentors.decorate }
+  expose(:past_events) { Event.past_events.decorate }
+  expose(:upcoming_events) { Event.upcoming_events.decorate }
 
   def index
   end
