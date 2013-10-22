a608f94d81f01d933a20c3a89cb7d04bb475fb52
Philip Crawford <philipharry@gmail.com>
2013-10-22 10:00:14 -0500

    Rectangular images and use medium for display

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ActiveRecord::Base
   validates :starts_at, timeliness: true
   validates :title, presence: true
 
-  has_attached_file :photo, styles: { medium: '300x300>', thumb: '100x100>' }
+  has_attached_file :photo, styles: { large: '600x400#', medium: '300x200#', thumb: '100x66#' }
 
   default_scope { order('events.starts_at desc') }
 
