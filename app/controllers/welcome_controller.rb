a32548cdb1162d356588fc0f13ff562fe0aa0621
Philip Crawford <philipharry@gmail.com>
2013-11-07 13:23:40 -0600

    board_members not mentors in about page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-  expose(:board_members) { Person.mentors.decorate }
+  expose(:board_members) { Person.board_members.decorate }
   expose(:past_events) { Event.past_events.without(raw_event).limit(3).decorate }
   expose(:event) { raw_event.decorate if raw_event }
   expose(:raw_event) { Event.next_or_last }
