9eb880bc2699aa602a5ec710620d298bb3302eb4
Mark McEahern <mark@mceahern.com>
2013-11-12 08:47:42 -0600

    Call Chamber.load! to load settings

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -3,4 +3,6 @@ class Settings
 
   source Rails.root.join('config', 'settings.yml'), namespace: Rails.env, override_from_environment: true
   source Rails.root.join('config', "credentials-#{Rails.env}.yml")
+
+  load!
 end
