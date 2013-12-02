c7e3b3677594da20454404c574c1b3c503cb9b9e
Philip Crawford <philipharry@gmail.com>
2013-12-02 17:21:37 -0600

    Change how ActiveAdmin looks up Events (id to slug)

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,31 +1,37 @@
 ActiveAdmin.register Event do
 
-  form html: { multipart: true } do |f|
-    f.inputs do
-      f.input :starts_at, :as => :just_datetime_picker
-      f.input :title
-      f.input :description, as: :html_editor
-      f.input :speaker
-      f.input :photo
-      f.input :venue_name
-      f.input :venue_street_address
-      f.input :venue_city
-      f.input :venue_state
-      f.input :venue_zip
-      f.input :host_name
-      f.input :photo_album_url
-      f.input :rsvp_url
-      f.input :published
-    end
+      controller do
+            def resource
+                  Event.where(slug: params[:id]).first!
+            end
+      end
 
-    f.actions
-  end
+      form html: { multipart: true } do |f|
+            f.inputs do
+                  f.input :starts_at, :as => :just_datetime_picker
+                  f.input :title
+                  f.input :description, as: :html_editor
+                  f.input :speaker
+                  f.input :photo
+                  f.input :venue_name
+                  f.input :venue_street_address
+                  f.input :venue_city
+                  f.input :venue_state
+                  f.input :venue_zip
+                  f.input :host_name
+                  f.input :photo_album_url
+                  f.input :rsvp_url
+                  f.input :published
+            end
 
-  index do
-    column :title
-    column :starts_at
-    column :published
-    column :rsvp_url
-    default_actions
-  end
+            f.actions
+      end
+
+      index do
+            column :title
+            column :starts_at
+            column :published
+            column :rsvp_url
+            default_actions
+      end
 end
