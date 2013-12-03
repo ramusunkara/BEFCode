d43ca4e491ef129705ba8e163b96c041633da90f
Zach Boatrite <zach.boatrite@gmail.com>
2013-12-03 10:56:28 -0600

    Fix indentation

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,37 +1,37 @@
 ActiveAdmin.register Event do
 
-      controller do
-            def resource
-                  Event.where(slug: params[:id]).first!
-            end
-      end
+  controller do
+    def resource
+      Event.where(slug: params[:id]).first!
+    end
+  end
 
-      form html: { multipart: true } do |f|
-            f.inputs do
-                  f.input :starts_at, :as => :just_datetime_picker
-                  f.input :title
-                  f.input :description, as: :html_editor
-                  f.input :speaker
-                  f.input :photo
-                  f.input :venue_name
-                  f.input :venue_street_address
-                  f.input :venue_city
-                  f.input :venue_state
-                  f.input :venue_zip
-                  f.input :host_name
-                  f.input :photo_album_url
-                  f.input :rsvp_url
-                  f.input :published
-            end
+  form html: { multipart: true } do |f|
+    f.inputs do
+      f.input :starts_at, :as => :just_datetime_picker
+      f.input :title
+      f.input :description, as: :html_editor
+      f.input :speaker
+      f.input :photo
+      f.input :venue_name
+      f.input :venue_street_address
+      f.input :venue_city
+      f.input :venue_state
+      f.input :venue_zip
+      f.input :host_name
+      f.input :photo_album_url
+      f.input :rsvp_url
+      f.input :published
+    end
 
-            f.actions
-      end
+    f.actions
+  end
 
-      index do
-            column :title
-            column :starts_at
-            column :published
-            column :rsvp_url
-            default_actions
-      end
+  index do
+    column :title
+    column :starts_at
+    column :published
+    column :rsvp_url
+    default_actions
+  end
 end
