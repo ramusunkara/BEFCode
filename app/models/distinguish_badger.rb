360965979aa654dbaf75e79d1d62ae806ad7739b
Mylara <mylari.cse@gmail.com>
2017-01-23 18:07:09 +0530

    Changes in distinguish badger page

--- app/models/distinguish_badger.rb
+++ app/models/distinguish_badger.rb
@@ -4,4 +4,6 @@ class DistinguishBadger < ActiveRecord::Base
   validates :last_name, presence: true
 
   has_attached_file :photo, styles: { large: '300x300', medium: '200x200', thumb: '100x100' }
+  validates_attachment :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
 end
